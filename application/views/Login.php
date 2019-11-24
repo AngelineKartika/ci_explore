447071bd017a978c4cbd72bb448bb8709186c443
verenvalenciaa <verenvalenciaa@gmail.com>
2019-11-24 22:22:20 +0700

    tes

--- application/views/Login.php
+++ application/views/Login.php
@@ -40,16 +40,16 @@
 
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email_pengguna">
+						<input class="input100" type="text" name="email_pengguna" placeholder="Email">
 						<span class="focus-input100"></span>
-						<span class="label-input100">Email</span>
+						
 					</div>
 
 
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="kata_sandi">
+						<input class="input100" type="password" name="kata_sandi" placeholder="Password">
 						<span class="focus-input100"></span>
-						<span class="label-input100">Password</span>
+						
 					</div>
 
 
