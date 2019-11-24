1ab28a60f4a09467b71002aa002ccd4b6ca6a705
verenvalenciaa <verenvalenciaa@gmail.com>
2019-11-24 21:47:48 +0700

    update direct logo

--- application/views/V_Register.php
+++ application/views/V_Register.php
@@ -109,7 +109,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light site-navbar-target" id="ftco-navbar">
 	    <div class="container">
         <!--logo-->
-            <a class="navbar-brand" href="index.html">EXPLORE</a>
+            <a class="navbar-brand" href="<?=base_url()?>Home">EXPLORE</a>
 	      <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
